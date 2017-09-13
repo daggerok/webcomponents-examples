d4cb1e108d354296e258816fac7b4506c3aa04bd
Maksim Kostromin <daggerok@gmail.com>
2017-09-14 01:17:03 +0300

    update package.json

--- 01-getting-started/gh-pages/postdeploy.bash
+++ 01-getting-started/gh-pages/postdeploy.bash
@@ -7,5 +7,4 @@ if ! [ -z "$1" ]; then
   sed -i -e "s/\<head\>\<base\ href=\"\/webcomponents-examples\/\"\/>/\<head\>/g" $1
   sed -i -e "s/\(href=\"\/webcomponents-examples\/\)/href=\"\//g" $1
   sed -i -e "s/\(src=\"\/webcomponents-examples\/\)/src=\"\//g" $1
-  rm -rf .nojekyll 404.html index.html-e
 fi
