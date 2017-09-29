8651335676f7f3d83eb653adbeac6a9008f5b1c8
Maksim Kostromin <daggerok@gmail.com>
2017-09-30 01:37:37 +0300

    fix base href

--- 02-toggled-button/gh-pages-deployment/postdeploy.bash
+++ 02-toggled-button/gh-pages-deployment/postdeploy.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # replace base-path with root:
-# /polymer-examples/ -> /
+# /webcomponents-examples/ -> /
 
 if ! [ -z "$1" ]; then
-  sed -i -e "s/\<head\>\<base\ href=\"\/polymer-examples\/\"\/>/\<head\>/g" $1
-  sed -i -e "s/\(src=\"\/polymer-examples\/\)/src=\"\//g" $1
-  sed -i -e "s/\(href=\"\/polymer-examples\/\)/href=\"\//g" $1
+  sed -i -e "s/\<head\>\<base\ href=\"\/webcomponents-examples\/\"\/>/\<head\>/g" $1
+  sed -i -e "s/\(src=\"\/webcomponents-examples\/\)/src=\"\//g" $1
+  sed -i -e "s/\(href=\"\/webcomponents-examples\/\)/href=\"\//g" $1
 fi
